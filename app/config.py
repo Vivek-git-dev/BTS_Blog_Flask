43fd3051e6b6c2611bb3e1c344b364fcbd92b3f9
Vivek-git-dev <58876246+Vivek-git-dev@users.noreply.github.com>
2026-01-04 21:57:19 +0530

    DB config update

--- app/config.py
+++ app/config.py
@@ -2,6 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///blog.db")
+    uri = os.getenv("DATABASE_URL", "sqlite:///blog.db")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_REGISTRATION_KEY = os.getenv("ADMIN_REGISTRATION_KEY")
